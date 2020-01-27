95c82a8ec860ac58cdf8b2778ebcdd521d391073
Wallace Cardoso <wallacefcardoso@gmail.com>
2020-01-27 18:54:32 -0300

    bug fix: concurrency dead lock

--- environment/devstack-devtest/state-based/state_based.py
+++ environment/devstack-devtest/state-based/state_based.py
@@ -13,10 +13,24 @@ class Message(object):
 
 
 class TestCase(object):
-    def __init__(self, event, args, messages):
+    def __init__(self, event, args=None, messages=None):
         self._event = event
         self._args = args
         self._messages = messages
+        self._func = None
+
+    def _func_undefined(self):
+        print('test-case function undefined')
+
+    @property
+    def func(self):
+        if self._func:
+            return self._func
+        return self._func_undefined
+
+    @func.setter
+    def func(self, func):
+        self._func = func
 
 
 class TestHandler(object):
@@ -30,6 +44,7 @@ class TestHandler(object):
         self.execution_cv = threading.Condition()
         self.status = TestHandler.NONE
         self.completion_cv = threading.Condition()
+        self.statem_ev = threading.Event()
 
 
 class TestSuiteExecution(threading.Thread):
@@ -40,20 +55,20 @@ class TestSuiteExecution(threading.Thread):
     def run(self):
         self._test_handler.status = TestHandler.STARTED
         for test in self._test_handler.tests:
+            self._test_handler.statem_ev.wait()
+            self._test_handler.statem_ev.clear()
             print('run test')
             self._test_handler.current_test = test
-            time.sleep(2)
+            test.func()
             with self._test_handler.execution_cv:
-                self._test_handler.execution_cv.notifyAll()
-            with self._test_handler.execution_cv:
+                self._test_handler.execution_cv.notify()
                 print('execution waiting states completion')
                 self._test_handler.execution_cv.wait()
-            with self._test_handler.execution_cv:
                 self._test_handler.status = TestHandler.STARTED if test != self._test_handler.tests[-1] else TestHandler.STOPPED
-                self._test_handler.execution_cv.notifyAll()
+                self._test_handler.execution_cv.notify()
         print('test-execution finished')
         with self._test_handler.completion_cv:
-            self._test_handler.completion_cv.notifyAll()
+            self._test_handler.completion_cv.notify()
 
 
 class StateMonitor(threading.Thread):
@@ -65,12 +80,11 @@ class StateMonitor(threading.Thread):
         while self._test_handler.status != TestHandler.STOPPED:
             print('monitor waiting test execution')
             with self._test_handler.execution_cv:
+                self._test_handler.statem_ev.set()
                 self._test_handler.execution_cv.wait()
-            print('monitor states')
-            time.sleep(2)
-            with self._test_handler.execution_cv:
-                self._test_handler.execution_cv.notifyAll()
-            with self._test_handler.execution_cv:
+                print('monitor states')
+                time.sleep(2)
+                self._test_handler.execution_cv.notify()
                 self._test_handler.execution_cv.wait()
         print('state-monitor stopped')
 
@@ -92,7 +106,7 @@ class MessageMonitor(threading.Thread):
         app = Flask('message-monitor')
         api = Api(app)
         api.add_resource(MessageMonitorApi, '/messages', resource_class_kwargs= { 'test_handler': self._test_handler })
-        server = multiprocessing.Process(target=app.run, kwargs={ 'port': 5052 })
+        server = multiprocessing.Process(target=app.run, kwargs={ 'port': 5055 })
         server.start()
         with self._test_handler.completion_cv:
             self._test_handler.completion_cv.wait()
@@ -104,7 +118,9 @@ class MessageMonitor(threading.Thread):
 if __name__ == '__main__':
     rabbitmq.falsify_bindings('localhost', 'admin', 'supersecret')
 
-    test_handler = TestHandler([1, 2, 3, 4, 5])
+    tests = [TestCase('create'), TestCase('build'), TestCase('stop')]
+
+    test_handler = TestHandler(tests)
     test_execution = TestSuiteExecution(test_handler)
     state_monitor = StateMonitor(test_handler)
     message_monitor = MessageMonitor(test_handler)
