38308cdaebec283f9ffcc16495ce9971a4ff919b
Wallace Cardoso <wallacefcardoso@gmail.com>
2020-02-19 20:31:39 -0300

    fix

--- environment/devstack-devtest/state-based/myenv.py
+++ environment/devstack-devtest/state-based/myenv.py
@@ -5,12 +5,28 @@ import requests
 import time
 import argparse
 import subprocess
+import yaml
 
 
+def load_env_config():
+    result = None
+    with open('tests.yaml', 'r') as tests_file:
+        result = yaml.load(tests_file, Loader=yaml.FullLoader)
+    return result
+
+
+ENV_CONFIG = load_env_config()
 EXTERNAL_PORT = 9000
-EXTERNAL_URL = '192.168.56.114'
-EXTERNAL_ADDRESS = str(EXTERNAL_URL) + ':' + str(EXTERNAL_PORT)
-_URL = 'http://' + EXTERNAL_ADDRESS + '/env'
+EXTERNAL_URL = 'localhost'
+PROFILE_CURRENT = ''
+
+
+def external_address():
+    return str(EXTERNAL_URL) + ':' + str(EXTERNAL_PORT)
+
+
+def url():
+    return 'http://' + external_address() + '/env'
 
 
 class EnvService(Resource):
@@ -40,16 +56,16 @@ def _wait_for_state(expected_state, url):
 
 
 def wait_env(test_id):
-    requests.post(_URL, json={'test_id': test_id})
-    _wait_for_state('env_up', _URL)
+    requests.post(url(), json={'test_id': test_id})
+    _wait_for_state('env_up', url())
 
 
 def wait_test_finish():
-    requests.put(_URL + '?state=' + 'terminated')
+    requests.put(url() + '?state=' + 'terminated')
 
 
 def _do_env_setup():
-    machines = []
+    machines = ENV_CONFIG['tests'][PROFILE_CURRENT]['env-api']['devices']
     for machine in machines:
         subprocess.run(['VBoxManage', 'controlvm', machine['name'], 'poweroff'])
         subprocess.run(['VBoxManage', 'snapshot', machine['name'], 'restore', machine['snapshot']])
@@ -58,15 +74,34 @@ def _do_env_setup():
 
 def wait_init():
     while True:
-        _wait_for_state('init', _URL)
+        _wait_for_state('init', url())
         _do_env_setup()
-        requests.put(_URL + '?state=env_up')
+        requests.put(url() + '?state=env_up')
+
+
+def prepare_vars(profile):
+    global EXTERNAL_PORT
+    global EXTERNAL_URL
+    global PROFILE_CURRENT
+
+    tests = ENV_CONFIG['tests']
+    if profile in tests:
+        print('Profile changed to ' + profile)
+        api = tests[profile]['env-api']
+        PROFILE_CURRENT = profile
+        if 'address' in api:
+            EXTERNAL_URL = api['port']
+        if 'port' in api:
+            EXTERNAL_PORT = api['port']
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('mode', type=str, choices=['server', 'client'])
+    parser.add_argument('--profile', type=str, required=False, default=None)
     args = parser.parse_args()
+    if args.profile:
+        prepare_vars(args.profile)
     if args.mode == 'server':
         app = Flask('environment-controller')
         api = Api(app)
