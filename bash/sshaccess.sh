99184221514c7678440b3d5a0f39cbed18cb58dc
Wallace Cardoso <wallacefcardoso@gmail.com>
2020-03-11 08:14:58 -0300

    ssh

--- bash/sshaccess.sh
+++ bash/sshaccess.sh
@@ -4,4 +4,6 @@ if [ ! -d ~/.ssh ]; then
     mkdir ~/.ssh
 fi
 chmod 700 ~/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyYjfgyPazTvGpd8OaAvtU2utL8W6gWC4JdRS1J95GhNNfQd657yO6s1AH5KYQWktcE6FO/xNUC2reEXSGC7ezy+sGO1kj9Limv5vrvNHvF1+wts0Cmyx61D2nQw35/Qz8BvpdJANL7VwP/cFI/p3yhvx2lsnjFE3hN8xRB2LtLUopUSVdBwACOVUmH2G+2BWMJDjVINd2DPqRIA4Zhy09KJ3O1Joabr0XpQL0yt/I9x8BVHdAx6l9U0tMg9dj5+tAjZvMAFfye3PJcYwwsfJoFxC8w/SLtqlFX7Ehw++8RtvomvuipLdmWCy+T9hIkl+gHYE4cS3OIqXH7f49jdJf jesse@spacey.local" > ~/.ssh/authorized_keys
+eval $(ssh-agent -s)
+ssh-add id_rsa
+cat id_rsa.pub > ~/.ssh/authorized_keys
