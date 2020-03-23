75f6660ecbb057bf7fee7218e148459300653d5c
Wallace <wallacefcardoso@gmail.com>
2020-03-23 20:53:20 -0300

    changes

--- bash/live.sh
+++ bash/live.sh
@@ -8,10 +8,12 @@ copy_key_files() {
     sudo rm -rf $DEST/.ssh
     sudo mkdir $DEST/.ssh
     sudo cp key/* $DEST/.ssh
+    sudo cat $DEST/.ssh/id_rsa.pub >> $DEST/.ssh/authorized_keys
     sudo chown -R $USER:$GROUP $DEST/.ssh
     sudo chmod 400 $DEST/.ssh/id_rsa
     sudo chmod 400 $DEST/.ssh/id_rsa.pub
     sudo chmod 400 $DEST/.ssh/config
+    sudo chmod 400 $DEST/.ssh/authorized_keys
 }
 
 
